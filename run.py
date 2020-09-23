15828e83b41be14d09451a1e820b87ccdd92f886
kevinkl2 <kkleung2@uci.edu>
2020-09-23 02:32:21 -0700

    add support for multiple data sets

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ def clear():
     else: 
         _ = system('clear')
 
-def generateEquation(variables, operators):
+def generateEquation(variables, values, operators):
     var = list(variables)
     random.shuffle(var)
     equation = []
@@ -19,37 +19,49 @@ def generateEquation(variables, operators):
     parenthesesCounter = len(variables)
     openParenthesesCounter = 0
 
+    for valueSet in values:
+        equationWithValues.append([])
+
     for i in var:
         if (parenthesesCounter > 0):
             openParens = random.randint(1,parenthesesCounter)
             equation += ["("]*openParens
-            equationWithValues += ["("]*openParens
+            for a in equationWithValues:
+                a += ["("]*openParens
             openParenthesesCounter += openParens
             parenthesesCounter -= openParens
 
         equation.append(i)
-        equationWithValues.append(variables[i])
+        for valueSet in values:
+            for a in equationWithValues:
+                a.append(valueSet[i])
 
         if (openParenthesesCounter > 0):
             closedParens = random.randint(0,openParenthesesCounter)
             equation += [")"]*closedParens
-            equationWithValues += [")"]*closedParens
+            for a in equationWithValues:
+                a += [")"]*closedParens
             openParenthesesCounter -= closedParens
 
         if (i != var[-1]):
             op = operators[random.randint(0,len(operators)-1)]
             equation.append(op)
-            equationWithValues.append(op)
+            for a in equationWithValues:
+                a.append(op)
         else:
             equation += [")"]*openParenthesesCounter
-            equationWithValues += [")"]*openParenthesesCounter
+            for a in equationWithValues:
+                a += [")"]*openParenthesesCounter
     
     return (equation, equationWithValues)
 
-def validateEquation(equationWithValues, goal):
-    return eval("".join(equationWithValues)) == goal
+def validateEquation(equationWithValues, goals):
+    for equationNum in range(0,len(equationWithValues)):
+        if eval("".join(equationWithValues[equationNum])) != goals[equationNum]:
+            return False
+    return True
 
-def findSolutions(variables, operators, goal, threadNumber):
+def findSolutions(variables, values, operators, goals, threadNumber):
     loopCounter = 0
 
     while loopCounter != 1000000000:
@@ -59,10 +71,10 @@ def findSolutions(variables, operators, goal, threadNumber):
         loopCounter += 1
         iterations[threadNumber] += 1
 
-        equation, equationWithValues = generateEquation(variables, operators)
+        equation, equationWithValues = generateEquation(variables, values, operators)
 
         try:
-            if validateEquation(equationWithValues, goal):
+            if validateEquation(equationWithValues, goals):
                 simplified = simplify("".join(equation))
                 if (simplified not in potentialSolutions):
                     potentialSolutions.append(simplified)
@@ -87,12 +99,13 @@ if __name__ == "__main__":
     iterations = defaultdict(int)
     potentialSolutions = []
     #[c*i + e*(a + b + d + g) + f + h, b*c + e*(a + d + f + i) + g + h, a + b + e*(c + d*(f + i)) + g + h, d*e*f + g + h*(a + i*(b + c)), a + b + c*i + e*(d + f + g) + h, a + b + e*(c*i + d*f) + g + h, d*(a + b + c*e + f + g) + h + i, e*(a + b*c + d + i) + f + g + h, e*(c + d*(a + b + i)) + f + g + h]
-    variables = {"a":"10", "b":"5", "c":"3", "d":"1.5", "e":"1.75", "f":"15", "g":"20", "h":"1.02", "i":"2"}
+    variables = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+    values = [{"a":"10", "b":"5", "c":"3", "d":"1.5", "e":"1.75", "f":"15", "g":"20", "h":"1.02", "i":"2"}]
     operators = ["+", "*"]
-    goal = 85.895
+    goals = [85.895]
     
     for threadNumber in range(1,threadCount+1):
-        threads.append(Thread(target=findSolutions, args=(variables, operators, goal, threadNumber,)))
+        threads.append(Thread(target=findSolutions, args=(variables, values, operators, goals, threadNumber,)))
     
     for thread in threads:
         thread.start()
